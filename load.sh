65dbdebdd185c8192c96c1636698b90ed621b294
Sebastien Jean Francois Campion <scampion@login05.leonardo.local>
2025-06-23 16:39:12 +0200

    update

---
+++ load.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+module load profile/deeplrn
+module load cudnn
+cp /leonardo/prod/spack/06/install/0.22/linux-rhel8-icelake/gcc-8.5.0/cuda-12.2.0-o6rr2unwsp4e4av6ukobro6plj7ceeos/nvvm/libdevice/libdevice.10.bc ./libdevice.10.bc
+
